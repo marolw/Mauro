8c3470880b6c3fb28d1259e8b34ae9ae1031a2d0
UC-IN-DISENO\UC-Industrial-diseño <marolw@gmail.com>
2020-03-20 20:27:09 +0100

    Añadido nuevo controlador manuela

---
+++ Mauro/Controllers/ManuelaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Mauro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManuelaController : ControllerBase
+    {
+        // GET: api/Manuela
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Zak", "Manuela2" };
+        }
+
+        // GET: api/Manuela/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "El numero 5";
+        }
+
+        // POST: api/Manuela
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Manuela/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
